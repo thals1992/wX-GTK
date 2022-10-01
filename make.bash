8e2526c379623c88d0b690d19f9a84b18c7f53de
josh <joshua.tee@gmail.com>
2022-10-01 05:37:14 -0400

    [REF] misc

---
+++ make.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# compile and run program
+#
+ulimit -c unlimited
+buildCommand="ninja"
+if [ "$1" != "" ]; then
+	buildCommand="ninja -j ${1}"
+fi
+echo ${buildCommand}
+
+cd build
+if ${buildCommand}; then
+	mv wxgtk ..
+	cd ..
+	# ./wxgtk
+else
+	compilation failed
+fi
